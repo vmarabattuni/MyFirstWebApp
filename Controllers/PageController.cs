5dd601e617c2a195939ac1415645c38350e1b690
Vamsi2911 <krishnamarabathuni@gmail.com>
2020-06-25 11:38:12 +0530

    Add project files.

---
+++ Controllers/PageController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MyFirstWebApp.Controllers
+{
+    public class PageController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public string Welcome(string name, int ID = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+        }
+    }
+}
